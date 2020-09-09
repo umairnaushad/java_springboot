ba0ef90fbadf500f62f20fa0e6ac08a6d75003e3
umair.naushad <umair.naushad@emaratech.ae>
2020-09-09 16:34:51 +0400

    Test

---
+++ src/test/java/SpringHelloTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SpringHelloTest {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+	assertEquals(0, 0, "0 x 0 must be 0");
+    }
+}
